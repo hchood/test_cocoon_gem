be1a3c008c71a4335b571c079631f8ccefd00f3f
Helen Hood <helen.c.hood@gmail.com>
2014-10-08 17:02:20 -0400

    Add accepts_nested_attributes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,10 @@
 class Workout < ActiveRecord::Base
   has_many :workout_exercises
   has_many :exercises, through: :workout_exercises
+  accepts_nested_attributes_for :workout_exercises,
+    reject_if: :all_blank,
+    allow_destroy: true
+  accepts_nested_attributes_for :exercises
 
   validates :name, presence: true
 end
--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,4 +1,6 @@
 class WorkoutExercise < ActiveRecord::Base
   belongs_to :workout
   belongs_to :exercise
+
+  accepts_nested_attributes_for :exercises, reject_if: :all_blank
 end
